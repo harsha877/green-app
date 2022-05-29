bebdacfa34a4e68a29111afc5c5354d2fa0a4694
Diksha Magotra <dikshamagotra@Dikshas-MacBook-Air.local>
2022-05-29 17:59:50 -0400

    Added bottom navigation for 3 different modules

---
+++ src/styles/bottomNav.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+const bottomNav=StyleSheet.create({
+    bottomNav: {
+        flex: 1;
+        alignItems: 'center';
+        justifyContent: 'center';
+    }
+    
+    textStyle: {
+        fontSize: 26;
+        fontWeight: 'bold';
+    }
+})
