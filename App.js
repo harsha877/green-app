ef57787a6b56bf8e7293ba4dae0f5c7886b7477a
harsha8777 <harsha877@gmail.com>
2022-05-31 18:25:23 -0400

    Database get check

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import MainContainer from './src/mainContainer';
+import { LogBox } from 'react-native';
+
 
 function App() {
+  LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core']);
   return (
     <MainContainer/>
   );
