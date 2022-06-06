cec84e18c4c6d0dfbc9d2ba0eee08f3452706842
krishna-22 <42892516+krishna-22@users.noreply.github.com>
2022-06-06 16:37:56 -0600

    Update customerLogin.js

--- src/views/customerLogin.js
+++ src/views/customerLogin.js
@@ -3,62 +3,10 @@ import { View, Text } from 'react-native';
 // import bottomNav from './styles/bottomNav';
 
 export default function CustomerLogin({ navigation }) {
-        const [email, setEmail] = useState("");
-        const [password, setPassword] = useState("");
- 
-   function handleLogin(e)
-   {
-      e.preventDefault();
-      console.log(email);
-      console.log(password);
-   }
-  return (
-    <View style={styles.container}>
-
-      <Image source={require('../assets/avatar.png')}/>
-        <TextInput
-        style={styles.TextInput}
-          placeholder="Email"
-          value={email}
-          onChangeText={(email) => setEmail(email)}
-        />
-
-        <TextInput
-        style={styles.TextInput}
-          placeholder="Password"
-          secureTextEntry={true}
-          value={password}
-          onChangeText={(password) => setPassword(password)}
-        />
- 
-        <Button style={styles.Button} onPress={handleLogin} title='LOGIN'/>
-    </View>
-  );
+        return(
+       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text
+                onPress={() => alert('This is the "Home" screen.')}
+                style={{ fontSize: 26, fontWeight: 'bold' }}>Customer Screen</Text>
+        </View>);
 }
- 
-const styles = StyleSheet.create({
-  container: {
-    display:"flex",
-    flexDirection:"column",
-    justifyContent:"center",
-    alignItems:"center",
-    marginTop:200
-  },
- 
-  TextInput:  {
-        height: 40,
-        margin:15,
-        width:250,
-        borderWidth: 1,
-        padding: 10,
-      },
-
- 
-  Button:
-  {
-        height: 40,
-        borderWidth: 1,
-        marginTop:10,
-        padding: 10,
-  }
-});
