5036375c553f8d591d8468b12b74b0813ba76437
krishna-22 <42892516+krishna-22@users.noreply.github.com>
2022-06-06 16:23:20 -0600

    Update customerLogin.js

--- src/views/customerLogin.js
+++ src/views/customerLogin.js
@@ -4,10 +4,62 @@ import { View, Text } from 'react-native';
 
 export default function CustomerLogin({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Customer Login</Text>
-        </View>
-    );
+        const [email, setEmail] = useState("");
+         const [password, setPassword] = useState("");
+ 
+   function handleLogin(e)
+   {
+      e.preventDefault();
+      console.log(email);
+      console.log(password);
+   }
+  return (
+    <View style={styles.container}>
+
+      <Image source={require('../assets/avatar.png')}/>
+        <TextInput
+        style={styles.TextInput}
+          placeholder="Email"
+          value={email}
+          onChangeText={(email) => setEmail(email)}
+        />
+
+        <TextInput
+        style={styles.TextInput}
+          placeholder="Password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(password) => setPassword(password)}
+        />
+ 
+        <Button style={styles.Button} onPress={handleLogin} title='LOGIN'/>
+    </View>
+  );
 }
+ 
+const styles = StyleSheet.create({
+  container: {
+    display:"flex",
+    flexDirection:"column",
+    justifyContent:"center",
+    alignItems:"center",
+    marginTop:200
+  },
+ 
+  TextInput:  {
+        height: 40,
+        margin:15,
+        width:250,
+        borderWidth: 1,
+        padding: 10,
+      },
+
+ 
+  Button:
+  {
+        height: 40,
+        borderWidth: 1,
+        marginTop:10,
+        padding: 10,
+  }
+});
