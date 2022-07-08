4c961eaacebdb8aa5eb7089da425c0362638f4f2
krishna-22 <42892516+krishna-22@users.noreply.github.com>
2022-07-08 17:59:49 -0600

    Delete HomeScreen.js

--- __tests__/HomeScreen.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from '../src/views/homeScreen'
-
