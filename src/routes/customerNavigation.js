7bc91269dd098f1e3fe9a131c8890c67d8c85bcd
“sakshamtandon” <saksham12356@gmail.com>
2022-07-19 23:39:27 -0400

    updated json for questionnarie screen and disable the header

--- src/routes/customerNavigation.js
+++ src/routes/customerNavigation.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { createStackNavigator } from '@react-navigation/stack';
-import QuestionScreen from '../views/questionScreen';
-import QuizScreen from '../views/quizScreen';
-import ResultsScreen from '../views/resultsScreen';
-import {USER_CUSTOM_QUIZ_HOMESCREEN, USER_CUSTOM_QUIZ_QUESTIONSCREEN, USER_CUSTOM_QUIZ_RESULTSCREEN}  
-    from "../constant/constants";
-import Customer from "../views/customer";
-import QuizInput from "../views/QuizInput";
-import CustomerViewScreen from "../views/customerViewScreen";
+import React from "react"
+import { createStackNavigator } from "@react-navigation/stack"
+import Customer from "../views/customer"
+import QuizInput from "../views/QuizInput"
+import CustomerViewScreen from "../views/customerViewScreen"
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export default function CustomerNavigator() {
-  return (
-    
-        <Stack.Navigator>
-            <Stack.Screen name='customer login' options={{headerShown: false}} component={Customer} />
-            <Stack.Screen name='customer question' options={{headerShown: false}} component={QuizInput} />
-            <Stack.Screen name='customer view' options={{headerShown: false}} component={CustomerViewScreen} />
-   
-        </Stack.Navigator>
-    
-  );
+	return (
+		<Stack.Navigator>
+			<Stack.Screen
+				name="customer login"
+				options={{ headerShown: false }}
+				component={Customer}
+			/>
+			<Stack.Screen
+				name="customer question"
+				options={{ headerShown: false }}
+				component={QuizInput}
+			/>
+			<Stack.Screen name="customer view" component={CustomerViewScreen} />
+		</Stack.Navigator>
+	)
 }
--- src/routes/quizScreenNavigation.js
+++ src/routes/quizScreenNavigation.js
@@ -1,25 +1,33 @@
-import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import QuestionScreen from '../views/questionScreen';
-import QuizScreen from '../views/quizScreen';
-import ResultsScreen from '../views/resultsScreen';
-import {USER_CUSTOM_QUIZ_HOMESCREEN, USER_CUSTOM_QUIZ_QUESTIONSCREEN, USER_CUSTOM_QUIZ_RESULTSCREEN}  
-    from "../constant/constants";
+import React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import QuestionScreen from "../views/questionScreen"
+import QuizScreen from "../views/quizScreen"
+import ResultsScreen from "../views/resultsScreen"
+import {
+	USER_CUSTOM_QUIZ_HOMESCREEN,
+	USER_CUSTOM_QUIZ_QUESTIONSCREEN,
+	USER_CUSTOM_QUIZ_RESULTSCREEN,
+} from "../constant/constants"
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export default function QuizScreenNavigator() {
-  return (
-    
-        <Stack.Navigator>
-            <Stack.Screen name={USER_CUSTOM_QUIZ_HOMESCREEN} options={{headerShown: false}} component={QuizScreen} />
-            <Stack.Screen name={USER_CUSTOM_QUIZ_RESULTSCREEN} options={{headerShown: false}} component={ResultsScreen} />
-            <Stack.Screen name={USER_CUSTOM_QUIZ_QUESTIONSCREEN} options={{headerShown: false}} component={QuestionScreen} />
-            
-            
-            
-        </Stack.Navigator>
-    
-  );
+	return (
+		<Stack.Navigator>
+			<Stack.Screen
+				name={USER_CUSTOM_QUIZ_HOMESCREEN}
+				options={{ headerShown: false }}
+				component={QuizScreen}
+			/>
+			<Stack.Screen
+				name={USER_CUSTOM_QUIZ_RESULTSCREEN}
+				component={ResultsScreen}
+			/>
+			<Stack.Screen
+				name={USER_CUSTOM_QUIZ_QUESTIONSCREEN}
+				component={QuestionScreen}
+			/>
+		</Stack.Navigator>
+	)
 }
